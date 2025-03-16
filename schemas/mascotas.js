51c91d97a30e462bf76e96eea1d9ff38ac3651a7
Cristian20044121 <crdcaro01@gmail.com>
2025-03-16 09:54:05 -0500

    Add mongoose dependency and create mascotas schema

---
+++ schemas/mascotas.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const mascotaSchema = new mongoose.Schema(
+  {
+    nombre: {
+      type: String,
+      required: true,
+    },
+    tipo: {
+      type: String,
+      required: true,
+    },
+    raza: {
+      type: String,
+      required: true,
+    },
+    edad: {
+      type: Number,
+      required: true,
+    },
+    descripcion: {
+      type: String,
+      required: true,
+    },
+    adoptado: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("mascotas", mascotaSchema);
